bb399b69bdfe12a0b6552edd510f39fea330855f
Ilian Gagliardi <46486795+iliangagliardi@users.noreply.github.com>
2022-04-08 17:10:13 +0200

    fixes to the gen.sh
    fixed the cross validation between client certs and intermediate ca

--- gen.sh
+++ gen.sh
@@ -5,7 +5,12 @@
 ##################################################################################
 ## CA ##
 #genCA=true to implement
-CAFILE="ca_chain.pem"
+CAFILE="ca-chain.pem"
+
+DAYS_ROOT_CA=1826
+DAYS_INTERMEDIATE_CA=730
+DAYS_SERVER_CERTS=365
+DAYS_CLIENT_CERTS=365
 
 
 ## INFOS ## 
@@ -22,7 +27,7 @@ ou_client="MongoDB-Client" #organization unit for client (drivers, agents)
 mongodb_client_hosts=( "mongodb-node1" "mongodb-node2" "mongodb-node3" "ilians-macbook" )
 
 ## SERVER LIST FOR CERTIFICATE GENERATION
-## The configuration is JSON based in order to manage subject alternative names in a dynamic way
+## The configuration is JSON based in order to manage subject alternative names in a dynamic way, modify it accordingly to your needs
 server_hosts_conf='{
         "servers":[
             {
@@ -56,7 +61,7 @@ mkdir gen
 cd gen
 
 echo "######################################################################################"
-echo "##### STEP 1: Generate CA "
+echo "##### STEP 1: Generate ROOT CA & INTERMEDIATE CA "
 
 # Generate CA config
 cat >> mongodb-ca.cfg <<EOF
@@ -94,31 +99,29 @@ ST=${ST}
 L=${L}
 O=${O}
 OU=MONGODB-CA
-CN=MONGODB-CA
-
 EOF
 
 # ca key
 openssl genrsa -out mongodb-ca.key 4096
 
 # cert ca gen
-openssl req -new -x509 -days 1826 -key mongodb-ca.key -out mongodb-ca.crt -config mongodb-ca.cfg -subj "$dn_prefix/CN=ROOTCA"
+openssl req -new -x509 -days $DAYS_ROOT_CA -key mongodb-ca.key -out mongodb-ca.crt -config mongodb-ca.cfg -subj "$dn_prefix/CN=ROOT-CA"
 
 # intermediate key
 openssl genrsa -out mongodb-ia.key 4096
 
 # certificate signing request for the intermediate certificate
-openssl req -new -key mongodb-ia.key -out mongodb-ia.csr -config mongodb-ca.cfg -subj "$dn_prefix/CN=CA-SIGNER"
+openssl req -new -key mongodb-ia.key -out mongodb-ia.csr -config mongodb-ca.cfg -subj "$dn_prefix/CN=INTERMEDIATE-CA"
 
 # Create the intermediate certificate
-openssl x509 -sha256 -req -days 730 -in mongodb-ia.csr -CA mongodb-ca.crt -CAkey mongodb-ca.key -set_serial 01 -out mongodb-ia.crt -extfile mongodb-ca.cfg -extensions v3_ca
+openssl x509 -sha256 -req -days $DAYS_INTERMEDIATE_CA -in mongodb-ia.csr -CA mongodb-ca.crt -CAkey mongodb-ca.key -set_serial 01 -out mongodb-ia.crt -extfile mongodb-ca.cfg -extensions v3_ca
 
 # Create the CA PEM file
 cat mongodb-ca.crt mongodb-ia.crt  > $CAFILE
 
 
 echo "######################################################################################"
-echo "##### STEP 4: Create server certificates"
+echo "##### STEP 2: Create server certificates"
 
 # Now create & sign keys for each mongod server 
 # Pay attention to the OU part of the subject in "openssl req" command
@@ -179,7 +182,7 @@ done
 
 
 
-echo "##### STEP 5: Create client certificates"
+echo "##### STEP 3: Create client certificates"
 # Now create & sign keys for each client
 # Pay attention to the OU part of the subject in "openssl req" command
 for host in "${mongodb_client_hosts[@]}"; do
